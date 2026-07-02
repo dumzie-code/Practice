293329cc378409a21e8c959430bdb4b2bf42d16a
Pohur Navin <navin.pohur@cygest.fr>
2026-07-02 21:04:16 +0400

    initial commit

---
+++ answers.py
@@ -0,0 +1,43 @@
+# classwork 1
+
+x =  int(input("Please enter your first number: "))
+y =  int(input("Please enter your second number: "))
+
+# if x > y:
+#     v = x - y
+#     print("answer is: ", v)
+
+# if y > x:
+#     w = y - x
+#     print ("Second number is greater than first number!!")
+#     print("answer is :", w)
+
+
+# version 2
+
+if x > y:
+    v = x - y
+    print("answer is: ", v)
+else:
+      w = y - x
+      print ("Second number is greater than first number!!")
+      print("answer is :", w)
+
+# classwork 2
+
+# name = input("Enter your name: ")
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+# sum = num1 + num2
+# product = num1 * num2
+
+# print("Student:", name)
+# print("Sum:", sum)
+# print("Product:", product)
+
+# if sum > 100:
+#     print("That's a big number!")
+# else:
+#     print("That's a small number.")
---
+++ test1.py
@@ -0,0 +1,90 @@
+
+#this is a comment
+
+#always add comments when you are coding. this helps the next person to understand your code
+
+#whenever you are declaring a variable, have a proper name not x, y, z... (e.g distance = , speed = )
+
+#pascal case: PascalCase, snake case: this_is_snake_case, Camel: avgSpeed (Each word is in capital except the first one)
+
+#to run your code, you can either click on the play button on the top right corner or in the terminal write python filename.py
+
+
+#to print something on the screen, we use the in-built function print("message")
+
+# variable = 8 / 5
+# print(int(variable))
+
+
+#to terminate a program from running
+# exit()
+
+#python has no command for declaring a variable
+# condition = 5 > 4
+# if condition:
+#     print("this is a computer science class")
+
+# print(5)
+
+# print("I am", 35, "years old.")
+
+
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+# x = 5
+# y = "John"
+# print(x, y)
+
+
+x = "cool!"
+def myfunc():
+  print("Python is " + x)
+
+# myfunc()
+
+def NewFunction():
+   multiplication = 6 * 5
+   print(multiplication)
+
+
+# NewFunction()
+
+def Fatine():
+   print("Hello friends!")
+#   print("ssss")
+
+# Fatine()
+
+
+#input => asks user for input
+
+# x = input("Enter name: ")
+
+# print("this is", x)
+
+#to convert a value to int
+
+# value = 4.25
+# b = int(value)
+# print(b)
+
+#
+# CLASSWORK
+# Write a program that asks the user for 2 inputs (integers)
+# the program will then check which input is greater and then subtract the smaller int from the greater int
+# the program will then print the answer
+#
+
+
+# CLASSWORK
+# Asks for the student's name.
+# Asks for two numbers.
+# Displays:
+# Student name
+# Sum
+# Product
+# If the sum is greater than 100, display: "That's a big number!"
